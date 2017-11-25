2301d3c7b9d207a451dd5d37a6b711fc14ffad7e
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-25 22:59:46 +0530

    added date js tuts

---
+++ test1.js
@@ -0,0 +1,15 @@
+var birthday = new Date(1985 , 7 , 21 , 15 , 25 );
+
+//To get the birthday year
+console.log("Birthday year : " + birthday.getFullYear());
+
+//TO get birthday month
+console.log("Birthday month : " + birthday.getMonth());
+
+//To get date of the month
+console.log(" birthay date : " + birthday.getDate());
+//To get Hours
+console.log("birthay hour : " + birthday.getHours());
+
+//To get time in milliseconds from 1 jan 1970
+console.log("Time : " + birthday.getTime());
